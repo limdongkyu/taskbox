4f2ad656862485e9d536cc20c305e02b449cb130
limdongkyu <hacker004@nate.com>
2021-11-20 02:27:48 +0900

    taskbox UI

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,12 @@
+import '../src/index.css';
+
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+  // controls: {
+  //   matchers: {
+  //     color: /(background|color)$/i,
+  //     date: /Date$/,
+  //   },
+  // },
 };
